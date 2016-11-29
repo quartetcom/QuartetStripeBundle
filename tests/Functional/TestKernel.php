37b299ccce089327a8ad1fd6ac08a39b2d206b09
Shota Hoshino <s-hoshino@quartetcom.co.jp>
2016-11-29 17:02:24 +0900

    Added functional tests

---
+++ tests/Functional/TestKernel.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Quartet\Bundle\StripeBundle\Functional;
+
+
+use Quartet\Bundle\StripeBundle\QuartetStripeBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class TestKernel extends Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new QuartetStripeBundle(),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config.yml');
+    }
+}
