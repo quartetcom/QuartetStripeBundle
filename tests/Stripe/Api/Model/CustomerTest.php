f7f18b2142809ad0ba087835d387429978241256
Shota Hoshino <s-hoshino@quartetcom.co.jp>
2016-12-05 15:35:06 +0900

    Added sources to Customer

--- tests/Stripe/Api/Model/CustomerTest.php
+++ tests/Stripe/Api/Model/CustomerTest.php
@@ -78,4 +78,13 @@ class CustomerTest extends ApiTestCase
         $this->assertInstanceOf(Charge::class, $charges[0]);
         $this->assertEquals(100, $charges[0]->value()->amount);
     }
+
+    public function testSources()
+    {
+        /* @var Card[] $sources */
+        $sources = $this->customer->sources()->toArray();
+        $this->assertCount(1, $sources);
+        $this->assertEquals('4242', $sources[0]->value()->last4);
+
+    }
 }
