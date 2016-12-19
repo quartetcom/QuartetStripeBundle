a7d72e84d0819b76abe4b9f2429e298246bec6cd
Shota Hoshino <sht.hshn@gmail.com>
2016-12-19 16:31:58 +0900

    Added an ability to localize stripe error message

---
+++ tests/StripeBundle/Functional/ErrorMessageLocalizationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Quartet\StripeBundle\Functional;
+
+
+use Quartet\StripeBundle\ErrorMessage;
+use Stripe\Error as StripeException;
+
+class ErrorMessageLocalizationTest extends WebTestCase
+{
+    /**
+     * @var ErrorMessage
+     */
+    private $errorMessage;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        static::bootKernel();
+
+        $this->errorMessage = static::$kernel->getContainer()->get('quartet.stripe.error_message');
+    }
+
+    public function test()
+    {
+        $error = new StripeException\Api('The card number is not a valid credit card number.');
+
+        $this->assertEquals('カード番号が不正です', $this->errorMessage->getLocalizedMessage($error, 'ja'));
+    }
+}
